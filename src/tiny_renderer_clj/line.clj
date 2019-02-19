b865799973dcca538c54b8a79cad7f2d8fd3da1b
Ivan Sharamet <isharamet@gmail.com>
2019-02-19 10:20:06 +0300

    Add fifth and final version of line drawing function

--- src/tiny_renderer_clj/line.clj
+++ src/tiny_renderer_clj/line.clj
@@ -53,3 +53,25 @@
                     {:err 0 :y y0 :g graphics}
                     (range x0 (inc x1)))]
     (:g res)))
+
+(defn draw-line-5 [graphics x0 y0 x1 y1 color]
+  (let [steep (< (Math/abs (- x0 x1)) (Math/abs (- y0 y1)))
+        [x0 y0 x1 y1] (if steep [y0 x0 y1 x1] [x0 y0 x1 y1])
+        [x0 y0 x1 y1] (if (> x0 x1) [x1 y1 x0 y0] [x0 y0 x1 y1])
+        dx (- x1 x0)
+        dy (- y1 y0)
+        derr2 (* (Math/abs dy) 2)
+        graphics (doto graphics (.setColor color))
+        res (reduce (fn [acc x]
+                      (let [yc (:y acc)
+                            [x y] (if steep [yc x] [x yc])
+                            err2 (+ (:err2 acc) derr2)
+                            [yn err2] (if (> err2 dx)
+                                        [(+ yc (if (> y1 y0) 1 -1)) (- err2 (* dx 2))]
+                                        [yc err2])]
+                        {:err2 err2
+                         :y yn
+                         :g (draw-pixel (:g acc) x y)}))
+                    {:err2 0 :y y0 :g graphics}
+                    (range x0 (inc x1)))]
+    (:g res)))
