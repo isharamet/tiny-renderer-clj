2a064eff86b6d4ddc13a7af870e8621e14e0f8cd
Ivan Sharamet <isharamet@gmail.com>
2019-02-28 10:28:12 +0300

    Add function for drawing colored triangle

--- src/tiny_renderer_clj/triangle.clj
+++ src/tiny_renderer_clj/triangle.clj
@@ -3,26 +3,30 @@
             [tiny-renderer-clj.line :as line])
   (:import (java.awt Color)))
 
-(defn draw-triangle-1
+(defn draw-triangle-1 [graphics vertices]
+  (let [sorted-vertices (sort-by second vertices)
+        [x0 y0 _] (first sorted-vertices)
+        [x1 y1 _] (second sorted-vertices)
+        [x2 y2 _] (last sorted-vertices)
+        height (- y2 y0)]
+    (reduce (fn [g y]
+              (let [second-half (> y y1)
+                    segment-height (if second-half (- y2 y1) (- y1 y0))
+                    ydiff (if second-half (- y2 y) (- y y0))
+                    alpha (double (/ ydiff height))
+                    beta (double (/ ydiff segment-height))
+                    [x0 x1 x2] (if second-half [x2 x1 x0] [x0 x1 x2])
+                    ax (+ x0 (Math/round (* alpha (- x2 x0))))
+                    bx (+ x0 (Math/round (* beta (- x1 x0))))]
+                (line/draw-line g ax y bx y)))
+            graphics
+            (range y0 y2))))
+
+(defn draw-triangle
+  ([graphics vertices color]
+   (doto graphics
+     (.setColor color)
+     (draw-triangle-1 vertices)))
   ([graphics vertices]
-   (let [sorted-vertices (sort-by second vertices)
-         [x0 y0 _] (first sorted-vertices)
-         [x1 y1 _] (second sorted-vertices)
-         [x2 y2 _] (last sorted-vertices)
-         height (- y2 y0)]
-     (reduce (fn [g y]
-               (let [second-half (> y y1)
-                     segment-height (if second-half (- y2 y1) (- y1 y0))
-                     ydiff (if second-half (- y2 y) (- y y0))
-                     alpha (double (/ ydiff height))
-                     beta (double (/ ydiff segment-height))
-                     [x0 x1 x2] (if second-half [x2 x1 x0] [x0 x1 x2])
-                     ax (+ x0 (Math/round (* alpha (- x2 x0))))
-                     bx (+ x0 (Math/round (* beta (- x1 x0))))]
-                 (line/draw-line g ax y bx y)))
-             graphics
-             (range y0 y2))))
-  ([graphics edges color]
-   (let [graphics (doto graphics (.setColor color))]
-     (draw-filled-triangle graphics edges))))
+   (draw-triangle graphics vertices (color/random-color))))
 
